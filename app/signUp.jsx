a6f6c60df677915e35bc4a53c909fc079072b818
tusharkumar2302 <tusharkumar27864@gmail.com>
2026-04-15 13:59:32 +0530

    Implement sign-up form with input fields, validation, and navigation

--- app/signUp.jsx
+++ app/signUp.jsx
@@ -1,14 +1,125 @@
-import { StyleSheet, Text, View } from 'react-native'
-import ScreenWrapper from '../components/ScreenWrapper'
+import { StyleSheet, View, Text, Pressable, Alert } from "react-native";
+import ScreenWrapper from "../components/ScreenWrapper";
+import { StatusBar } from "expo-status-bar";
+import BackButton from "../components/BackButton";
+import { useRouter } from "expo-router";
+import { hp, wp } from "../helpers/common";
+import { theme } from "../constants/theme";
+import Input from "../components/Input";
+import Icon from "../assets/icons";
+import { useRef, useState } from "react";
+import Button from "../components/Button";
 
-const signUp = () => {
+const SignUp = () => {
+  const router = useRouter();
+  const nameRef = useRef("");
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const [loading, setLoading] = useState(false);
+
+  const onSubmit = async () => {
+    if (!emailRef.current || !passwordRef.current) {
+      Alert.alert("Sign up", "Please fill all the fields!");
+      return;
+    }
+  };
+
   return (
-    <ScreenWrapper>
-      <Text>signUp</Text>
+    <ScreenWrapper bg={"white"}>
+      <StatusBar style="dark" />
+      <View style={styles.container}>
+        <BackButton router={router} />
+
+        {/* Welcome Text */}
+        <View>
+          <Text style={styles.welcomeText}>Let's,</Text>
+          <Text style={styles.welcomeText}>Get Started</Text>
+        </View>
+
+        {/* Form */}
+        <View style={styles.form}>
+          <Text style={{ fontSize: hp(1.5), color: theme.colors.text }}>
+                 Please fill the details to create an account
+          </Text>
+          <Input
+            icon={<Icon name="user" size={26} strokeWidth={1.6} />}
+            placeholder="Enter your name"
+            onChangeText={(value) => {
+              nameRef.current = value;
+            }}
+          />
+          <Input
+            icon={<Icon name="mail" size={26} strokeWidth={1.6} />}
+            placeholder="Enter your email"
+            onChangeText={(value) => {
+              emailRef.current = value;
+            }}
+          />
+          <Input
+            icon={<Icon name="lock" size={26} strokeWidth={1.6} />}
+            placeholder="Enter your password"
+            secureTextEntry
+            onChangeText={(value) => {
+              passwordRef.current = value;
+            }}
+          />
+
+          {/* Button */}
+          <Button title={"Sign up"} loading={loading} onPress={onSubmit} />
+        </View>
+
+        {/* Footer */}
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>Already have an account!</Text>
+          <Pressable onPress={()=>{router.push('login')}}>
+            <Text
+              style={[
+                styles.footerText,
+                {
+                  color: theme.colors.primaryDark,
+                  fontWeight: theme.fonts.semibold,
+                },
+              ]}
+            >
+              Login
+            </Text>
+          </Pressable>
+        </View>
+      </View>
     </ScreenWrapper>
-  )
-}
+  );
+};
 
-export default signUp
+export default SignUp;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 45,
+    paddingHorizontal: wp(5),
+  },
+  welcomeText: {
+    fontSize: hp(4),
+    fontWeight: theme.fonts.bold,
+    color: theme.colors.text,
+  },
+  form: {
+    gap: 25,
+  },
+  forgotPassword: {
+    textAlign: "right",
+    fontWeight: theme.fonts.semibold,
+    color: theme.colors.text,
+  },
+  footer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 5,
+  },
+  footerText: {
+    textAlign: "center",
+    color: theme.colors.text,
+    fontSize: hp(1.6),
+  },
+});
